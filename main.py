6c796f5723adc731e1741ee65459c654ce2da150
anettoph <anettoph@mail.ru>
2021-02-21 15:17:19 +0300

    1st

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import discord
 import os 
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+	response = requests.get
+
 
 @client.event
 async def on_ready():
 	print("Hello! I'm {0.user}".format(client))
 
 @client.event
-async def on_messages(message):
+async def on_message(message):
 	if message.author == client.user:
 		return
 
